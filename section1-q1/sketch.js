0ead355663a3df8d190c457b253cb35e4d4d0aac
2001081h <2001081h@stu.kobe-u.ac.jp>
2021-10-12 12:23:06 +0900

    section1-q1 changes
    the mark of kobe-city

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -2,12 +2,13 @@
 function setup(){
   createCanvas(200, 200);
   background(255);
-  
+
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
+  stroke(0,128,9)
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
     arc(100 - 28, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
 
