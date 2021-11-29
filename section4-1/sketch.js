0730bf3789375c0e33f9d38f6261142d9cffabc3
2001081h <2001081h@stu.kobe-u.ac.jp>
2021-11-29 17:08:51 +0900

    中途半端です

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -6,13 +6,17 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
   }
   console.log(scores);
 
   // 合計を計算する
   let sum = 0;
   for(let i = 0; i < scores.length; i++){
+    const dx = width / scores.length;
+    const h =height*scores[i]/100;
+    fill(128)
+    rect(i*dx,height - h,dx,h);
     sum += scores[i];
   }
   console.log(sum);
@@ -34,7 +38,7 @@ function setup(){
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
   const n = 10;
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
-  
+
   noStroke();
 
   for(let i = 0; i < scores.length; i++){
