ad3f959e38afe67f4e8f947698befcd280e485e7
2001081h <2001081h@stu.kobe-u.ac.jp>
2021-11-29 18:19:05 +0900

    途中です

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,35 +1,13 @@
-// 「キーボード操作に反応する」
-let x, y;
-
-function setup(){
-  createCanvas(windowWidth, windowHeight);
-  x = width / 2;
-  y = height / 2;
+let img;
+function preload() {
+  img = loadImage('fashion_usugi_girl.png');
 }
 
-function draw(){
-  background(160, 192, 255);
+function setup() {
+  createCanvas(500, 500);
 
-//画像
-document.write('<img src="fashion_usugi_girl.png">')
-
-  if(keyIsDown(LEFT_ARROW)){ x -= 5; }
-  if(keyIsDown(RIGHT_ARROW)){ x += 5; }
-  if(keyIsDown(UP_ARROW)){ y -= 5; }
-  if(keyIsDown(DOWN_ARROW)){ y += 5; }
-  if(keyIsDown("A".charCodeAt(0))){ x+= 10; }
-  if(keyIsDown(" ".charCodeAt(0))){ x-= 10; }
-}
-
-// イベントハンドラを使用するパターン
-// function keyPressed(){
-//   if(keyCode == LEFT_ARROW){ x -= 5; }
-//   else if(keyCode == RIGHT_ARROW){ x+= 5; }
-//   else if(keyCode == DOWN_ARROW){ y += 5; }
-//   else if(keyCode == UP_ARROW){ y -= 5; }
-//   else if(key == "A"){ x += 10; }
-// }
-
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+  // 表示する画像の左上隅の座標を (10, 10) にする
+  // 表示する画像の幅と高さを 100 x 100 にする
+  imageMode(CORNER);
+  image(img, 10, 10, 100, 100);
 }
